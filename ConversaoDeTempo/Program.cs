d549e7b5a204d4a7737e6c5cef613f6f2c209440
Rafael <rafa.viturino94@gmail.com>
2019-09-16 00:02:41 -0300

    Projeto iniciando, mostrando a conversao

---
+++ ConversaoDeTempo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConversaoDeTempo {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Informe a quantidade de segundos");
+            int n = int.Parse(Console.ReadLine());
+
+            int horas = n / 3600;
+            int resto = n % 3600;
+            int minutos = resto / 60;
+            int segundos = resto % 60;
+
+            Console.WriteLine("Os segundos convertidos para horas:minutos:segundos é: " + horas + ":" + minutos + ":" + segundos);
+            Console.ReadLine();
+
+        }
+    }
+}
